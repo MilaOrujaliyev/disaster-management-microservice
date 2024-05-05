96d469a7f2ba4e22035b57d82a524643e27459ac
Mila-U <88654285+Mila-U@users.noreply.github.com>
2024-05-05 18:00:30 +0300

    CrossOrigin is added

--- src/main/java/org/afetankanet/disastermanagementmicroservice/controller/EarthquakeController.java
+++ src/main/java/org/afetankanet/disastermanagementmicroservice/controller/EarthquakeController.java
@@ -5,13 +5,11 @@ import org.afetankanet.disastermanagementmicroservice.service.EarthquakeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.time.LocalDateTime;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/earthquakes")
 public class EarthquakeController {
